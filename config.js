9bcfc0a9bafd404761e0475c5c2114d99a5d6ca0
Daryl <mrdaryldauphin@gmail.com>
2020-11-05 08:33:12 -0500

    setting up our react app

---
+++ config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "darylsnotes"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://ywroejrnla.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_uGj1gpBw3",
+      APP_CLIENT_ID: "7b7sagatbqg6g6oj3s0h16mjk5",
+      IDENTITY_POOL_ID: "us-east-1:720688c8-22f3-443b-9451-2295b7df373b"
+    }
+  };
